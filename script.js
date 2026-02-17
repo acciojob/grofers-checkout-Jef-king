743be67dd1c60e8c6017004137f73c892de828f8
Jef-king <147884623+Jef-king@users.noreply.github.com>
2026-02-17 21:00:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ const getSum = () => {
     newCell.textContent = sum;
     newCell.id = "ans";
 	newCell.className="prices";
+	newCell.classList.add('prices');
     newRow.appendChild(newCell);
     table.appendChild(newRow);
   
