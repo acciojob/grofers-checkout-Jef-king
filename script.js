e250a72a08ba397faba06f86403a0acc5c917660
Jef-king <147884623+Jef-king@users.noreply.github.com>
2026-02-17 20:49:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,19 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
+	const prices = document.querySelectorAll('.prices');
+    let sum = 0;
+    prices.forEach(price => {
+        sum += parseint(price.textContent);
+    });
+	alert(sum);
+    const table = document.querySelector('table');
+    const newRow = document.createElement('tr');
+    const newCell = document.createElement('td');
+    newCell.textContent = sum;
+    newCell.id = "ans";
+    newRow.appendChild(newCell);
+    table.appendChild(newRow);
   
 };
 
